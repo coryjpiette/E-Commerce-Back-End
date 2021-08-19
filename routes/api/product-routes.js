47e1d135a17e16c4628e4c917c8ecf76cd9f2ec3
Cory Piette <coryjpiette@gmail.com>
2021-08-19 19:15:51 -0400

    still working  on product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,4 +1,6 @@
   
+
+
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
@@ -36,7 +38,7 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+           attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
         include: [
             {
                 model: Category,
@@ -48,4 +50,43 @@ router.get('/:id', (req, res) => {
             }
         ]
     })
-        .the
+    
+
+    .then(dbProductData => {
+           if (!dbProductData) {
+            res.status(404).json({ message: 'No product found with this id' });
+            return;
+        }
+        res.json(dbProductData);
+    })
+    .catch(err => {
+        console.log(err);
+          res.status(500).json(err);
+    });
+});
+
+// create new product
+router.post('/', (req, res) => {
+
+    Product.create(req.body)
+        .then((product) => {
+            // Create pairings if there's a product tag
+
+            if (req.body.tagIds.length) {
+                const productTagIdArr = req.body.tagIds.map((tag_id) => {
+                    return {
+                        product_id: product.id,
+                        tag_id,
+                    };
+                });
+                  return ProductTag.bulkCreate(productTagIdArr);
+            }
+            // Response if no product tag
+            res.status(200).json(product);
+        })
+        .then((productTagIds) => res.status(200).json(productTagIds))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
