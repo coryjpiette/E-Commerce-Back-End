83d6bfee28dde581286c302a5e6b95815da94ded
Cory Piette <coryjpiette@gmail.com>
2021-08-19 21:24:37 -0400

    continuing server.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -63,3 +63,48 @@ router.post('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.put('/:id', (req, res) => {
+    // Update tags by ID
+
+    Tag.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbTagData => {
+            if (!dbTagData[0]) {
+                res.status(404).json({ message: 'Invalid ID. No tag found' });
+                return;
+            }
+            res.json(dbTagData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    // Delete by ID
+    Tag.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbTagData => {
+            if (!dbTagData) {
+                res.status(404).json({ message: 'Invalid Id. Not tag found.' });
+                return;
+            }
+            res.json(dbTagData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+module.exports = router;
+
