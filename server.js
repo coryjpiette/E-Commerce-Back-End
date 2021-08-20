950e0718a6601d43d859ebb1eae0c8a8ad6954a1
Cory Piette <coryjpiette@gmail.com>
2021-08-19 21:25:21 -0400

    format cleanup

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const routes = require('./routes');
 
 // import sequelize
 
-  const sequelize = require('./config/connection');
+const sequelize = require('./config/connection');
 
-  const app = express();
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-    app.use(express.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 // sync sequelize models to the database
 
-    sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
